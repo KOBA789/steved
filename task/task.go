c180da4bed3f6748f041aeb50b2fb70c90e3c0f6
Hidekazu Kobayashi <kobahide789@gmail.com>
2017-08-02 02:05:01 +0900

    add slack integration

--- task/task.go
+++ task/task.go
@@ -2,6 +2,7 @@ package task
 
 import (
 	"encoding/json"
+	"github.com/ashwanthkumar/slack-go-webhook"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
@@ -14,17 +15,59 @@ type TaskMap map[string]Task
 type Task struct {
 	Image string
 	Cmd   []string
+	Slack *string
+}
+
+func (t *Task) buildSlackPayload(title *string, text *string, color *string) slack.Payload {
+	attachment := slack.Attachment {
+		Title: title,
+		Text: text,
+		Color: color,
+	}
+	return slack.Payload {
+		Username: "steved",
+		Attachments: []slack.Attachment{attachment},
+	}
+}
+
+func (t *Task) buildSlackPayloadStarted(name string) slack.Payload {
+	title := "[STARTED] " + name
+	color := "#28d7e5"
+	return t.buildSlackPayload(&title, nil, &color)
+}
+
+func (t *Task) buildSlackPayloadFailed(name string, err error) slack.Payload {
+	title := "[FAILED] " + name
+	text := err.Error()
+	color := "danger"
+	return t.buildSlackPayload(&title, &text, &color)
+}
+
+func (t *Task) buildSlackPayloadSucceed(name string) slack.Payload {
+	title := "[SUCCEED] " + name
+	color := "good"
+	return t.buildSlackPayload(&title, nil, &color)
+}
+
+func (t *Task) NotifyToSlack(payload slack.Payload) {
+	if t.Slack == nil {
+		return
+	}
+	slack.Send(*t.Slack, "", payload)
 }
 
 func (t *Task) Spawn(name string, env []string) error {
+	t.NotifyToSlack(t.buildSlackPayloadStarted(name))
 	ctx := context.Background()
 	cli, err := client.NewEnvClient()
 	if err != nil {
+		t.NotifyToSlack(t.buildSlackPayloadFailed(name, err))
 		return err
 	}
 
 	_, err = cli.ImagePull(ctx, t.Image, types.ImagePullOptions{})
 	if err != nil {
+		t.NotifyToSlack(t.buildSlackPayloadFailed(name, err))
 		return err
 	}
 
@@ -36,13 +79,16 @@ func (t *Task) Spawn(name string, env []string) error {
 		AutoRemove: true,
 	}, nil, "steved-job-"+name)
 	if err != nil {
+		t.NotifyToSlack(t.buildSlackPayloadFailed(name, err))
 		return err
 	}
 
 	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		t.NotifyToSlack(t.buildSlackPayloadFailed(name, err))
 		return err
 	}
 
+	t.NotifyToSlack(t.buildSlackPayloadSucceed(name))
 	return nil
 }
 
